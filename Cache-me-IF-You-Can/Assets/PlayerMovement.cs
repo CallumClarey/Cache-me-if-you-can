05394c3e428b26015e72820c55e99067cb2bc972
CallumClarey <callumclarey21@gmail.com>
2025-03-18 00:12:12 +0000

    Player Animation Implementation
    Implemented Full animation
    Changed Player collision to make it smoother

--- Cache-me-IF-You-Can/Assets/PlayerMovement.cs
+++ Cache-me-IF-You-Can/Assets/PlayerMovement.cs
@@ -5,10 +5,16 @@ public class PlayerMovement : MonoBehaviour
     //------------------------------
     //Class attributes 
     //-----------------------------
+    //Determines player movement speed
     public float movementSpeed;
 
+    //referance to the player rigid body
     public Rigidbody2D body;
 
+    //referance to the player animation controller
+    public Animator animator;
+
+    //Vector to store movement values 
     Vector2 movement;
 
     // Update is called once per frame
@@ -17,6 +23,11 @@ public class PlayerMovement : MonoBehaviour
         //returns a value between -1 and 1 on both axis 
         movement.x =Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        //All code used to handle animations
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
     }
 
     private void FixedUpdate()
